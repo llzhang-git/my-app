861228d33d6b4e7c433a5fbc05c84adce9adbd3a
zhanglulu <zhanglulu@ata.net.cn>
2023-02-09 09:42:53 +0800

    新增： 读取图片

--- my-project/src/app/app.component.ts
+++ my-project/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { imageData } from '../imageData';
+declare var EXIF: any;
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`,
-  styles: ['']
+    template: `
+        <img #img src="{{data}}" style="display: block;width :10px"/> 
+        <button (click)="getDataClick()">Get Data</button>
+        <div>{{output | json}}</div>
+  `
 })
 export class AppComponent {
   title = 'my-project';
+  @ViewChild('img') imgEl: ElementRef;
+  data = imageData;
+
+  output: string;
+
+  private getExif() {
+    let allMetaData: any;
+    EXIF.getData(<HTMLImageElement>this.imgEl.nativeElement, function () {
+      // `this` is provided image, check with `console.log(this)`
+      allMetaData = EXIF.getAllTags(this);
+    });
+
+    this.output = allMetaData;
+  }
+
+  getDataClick() {
+    this.getExif();
+  }
 }
